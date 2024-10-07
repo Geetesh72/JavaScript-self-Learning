587ad037438b72f3e10adb12f5d206206ba77083
Geetesh72 <125372539+Geetesh72@users.noreply.github.com>
2024-10-07 19:31:55 +0000

    Variables

---
+++ Basics_of_js/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 23443;
+let accountEmail = "otisMilburn@gmail.com";
+var accountPassword = "maeve";
+accountCity = "Mordale Secondary ";
+let accountState;
+
+accountEmail = "eric@gmail.com";
+accountPassword = "adam ";
+accountCity =  "Canvendish";
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity]);
