c9147495556686a5cf82899d477f63b591f48da2
Geetesh72 <125372539+Geetesh72@users.noreply.github.com>
2024-10-07 19:32:20 +0000

    DataTypes

---
+++ Basics_of_js/dataType.js
@@ -0,0 +1,19 @@
+//  we have to write code in better readability format so that anyone can refer this
+"use strict";
+
+// alert(2+3) we using node js not browser
+
+console.log("this is geetesh ") // we can avoid use semicolon 
+console.log(typeof null )
+console.log(typeof(undefined));
+/*
+Type of dataType in js 
+integer => 2^53 
+bigint
+string 
+boolean 
+null =>standalone value
+undefined
+symbol  => unique 
+*/ 
+//  object 
