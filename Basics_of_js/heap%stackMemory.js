3a8553537096b5e7d0a1b1e37b91ee80eeaf954c
Geetesh72 <125372539+Geetesh72@users.noreply.github.com>
2024-10-09 01:13:33 +0530

    Create heap%stackMemory.js

---
+++ Basics_of_js/heap%stackMemory.js
@@ -0,0 +1,23 @@
+// all primitive value access by stack 
+// primitve -> by stack
+
+let curr = "job less"
+let inNextMonth = "At decent Company"
+inNextMonth = "At Google"
+console.log(inNextMonth)
+console.log(curr)
+
+
+let user = {
+    email: "jojo@gmail",
+    upi : "use@ybl"
+}
+let user2 =user
+console.log(user)
+user2.email = "myemail@3456t"
+user2.upi = "ajay@ybl"
+console.log(user2)
+
+
+
+
