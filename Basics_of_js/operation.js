e3d84e21a4fccad23ebf1c733cdd69e42d052e9b
Geetesh72 <125372539+Geetesh72@users.noreply.github.com>
2024-10-08 15:19:08 +0530

    Create operation.js
    All abouts js opration

---
+++ Basics_of_js/operation.js
@@ -0,0 +1,29 @@
+let val = 2 
+let negVal = -val
+let a  = 245 
+let b = 34
+console.log(a-b)
+console.log(a+b)
+console.log(a/b)
+console.log(a%b)
+console.log(a*b)
+console.log(a^b)
+console.log(a&b)
+console.log(a|b)
+console.log(a**b)
+
+let s1 = "hello"
+let s2 = " Geetesh"
+let s3 = s1+s2
+console.log(s3)
+
+console.log("1"+2)
+console.log(2+4+"4")
+console.log("1"+2+2)
+
+/*This lecture Basically Focus on how to write with Better readability 
+not to */
+
+
+
+
