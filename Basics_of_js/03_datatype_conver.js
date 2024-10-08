f943f06e4442900446f30fccc395a3b7b83daa2d
Geetesh72 <125372539+Geetesh72@users.noreply.github.com>
2024-10-08 15:18:20 +0530

    Create 03_datatype_conver.js

---
+++ Basics_of_js/03_datatype_conver.js
@@ -0,0 +1,28 @@
+let score = "2435tg"
+// let score = true
+/*
+score can be boolean 
+33=> 33
+24rgb => NaN
+*/
+console.log(typeof score);
+console.log(typeof (score))
+
+
+// converting string to number 
+let valueIn = Number(score);
+console.log(typeof valueIn)
+console.log(valueIn)
+
+console.log("Now let Explore More Conversions")
+console.log("Boolean  to String Conversion\n")
+let isLogged = "Its me who logged in "
+let booleanIsloggedIn = Boolean(isLogged)
+console.log(booleanIsloggedIn)
+console.log(typeof booleanIsloggedIn)
+
+console.log("integer to Boolean")
+let num = 7108
+let toString = String(num)
+console.log(toString)
+console.log(typeof toString)
